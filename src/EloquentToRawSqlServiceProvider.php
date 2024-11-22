9bdbfeca414d0294c471b7e136fa6d71325a2fd8
Federico Liva <federico.liva@espero.it>
2024-11-22 10:35:57 +0100

    Fix some binds were not interpreted correctly

--- src/EloquentToRawSqlServiceProvider.php
+++ src/EloquentToRawSqlServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace Fliva\EloquentToRawSql;
 
-use DateTime;
 use Doctrine\SqlFormatter\SqlFormatter;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class EloquentToRawSqlServiceProvider extends ServiceProvider
 {
@@ -18,28 +18,26 @@ class EloquentToRawSqlServiceProvider extends ServiceProvider
          * This method converts an SQL query into a raw SQL string
          * by replacing placeholders with the corresponding bindings.
          *
-         * Original idea by @therobfonz (https://twitter.com/kirschbaum_dev/status/1418590965368074241)
-         * Refined by BinaryKitten (https://gist.github.com/BinaryKitten/2873e11daf3c0130b5a19f6b94315033)
+         * Original idea by @PyaeSoneAungRgn (https://github.com/PyaeSoneAungRgn/to-raw-sql)
          *
          * @return string The final SQL query with the bound values replaced.
          */
         QueryBuilder::macro('toRawSql', function () {
-            return array_reduce(
-                $this->getBindings(),
-                static function ($sql, $binding) {
-                    if ($binding instanceof DateTime) {
-                        $binding = $binding->format('Y-m-d H:i:s');
-                    }
+            $bindings = $this->getBindings();
 
-                    return preg_replace(
-                        '/\?/',
-                        is_string($binding) ? "'" . $binding . "'" : $binding,
-                        str_replace('"', '', $sql),
-                        1
-                    );
-                },
-                $this->toSql()
-            );
+            foreach ($bindings as $key => $value) {
+                if ($value instanceof DateTimeInterface) {
+                    $bindings[$key] = "'{$value->format('Y-m-d H:i:s')}'";
+                } elseif (is_string($value)) {
+                    $bindings[$key] = "'{$value}'";
+                } elseif (is_bool($value)) {
+                    $bindings[$key] = $value ? 'true' : 'false';
+                } elseif (is_null($value)) {
+                    $bindings[$key] = 'NULL';
+                }
+            }
+
+            return Str::replaceArray('?', $bindings, $this->toSql());
         });
 
         /**
